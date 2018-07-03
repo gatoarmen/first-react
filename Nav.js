9a8d1403403034ccd26ed19e894a5eedff1318b0
esteban <esteban_armendariz@hotmail.com>
2018-07-03 14:05:32 -0500

    b

--- Nav.js
+++ Nav.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./Nav.css";
-
 
 const Nav = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <a className="navbar-brand" href="/">
-      Click Game
+      React Reading List
     </a>
   </nav>
 );
